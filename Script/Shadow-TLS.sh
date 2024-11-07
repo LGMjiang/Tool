467912ecc60be590b4675247605c10c5a0bfe206
LGMjiang <66723084+LGMjiang@users.noreply.github.com>
2024-11-07 12:03:19 +0800

    Update Shadow-TLS.sh

--- Script/Shadow-TLS.sh
+++ Script/Shadow-TLS.sh
@@ -62,9 +62,9 @@ generate_client_config() {
     return
   else
     ss_port=$(grep '"server_port"' /etc/ss-rust/config.json | sed 's/[^0-9]*\([0-9]*\).*/\1/') # 获取 ss-rust 服务的端口号
-    ss_transmission_mode=$(grep '"mode"' /etc/ss-rust/config.json | sed 's/.*"mode": "\(.*\)",/\1/') # 获取 ss-rust 的传输模式
-    ss_password=$(grep '"password"' /etc/ss-rust/config.json | sed 's/.*"password": "\(.*\)",/\1/')
-    encryption_method=$(grep '"method"' /etc/ss-rust/config.json | sed 's/.*"method": "\(.*\)",/\1/')
+    ss_transmission_mode=$(grep '"mode"' /etc/ss-rust/config.json | sed 's/.*"mode": "\(.*\)",/\1/') # 获取 ss-rust 服务的传输模式
+    ss_password=$(grep '"password"' /etc/ss-rust/config.json | sed 's/.*"password": "\(.*\)",/\1/') # 获取 ss-rust 服务的密码
+    encryption_method=$(grep '"method"' /etc/ss-rust/config.json | sed 's/.*"method": "\(.*\)",/\1/') # 获取 ss-rust 服务的加密方式
   fi
 
   # 选择是否开启 udp
@@ -170,6 +170,20 @@ update_shadow_tls() {
 
 # 安装 Shadow-TLS 函数
 install_shadow_tls() {
+  local ss_port=""
+  # 检查 Shadowsocks-Rust 的服务和配置文件
+  if ! ssserver -V > /dev/null 2>&1; then
+    echo "无法继续安装 Shadow-TLS 服务！"
+    echo "未检测到 Shadowsocks-Rust 服务！"
+    exit 1
+  elif [[ ! -e /etc/ss-rust/config.json ]]; then
+    echo "无法继续安装 Shadow-TLS 服务！"
+    echo "未检测到 Shadowsocks-Rust 的配置文件！请检查其配置文件是否在/etc/ss-rust/目录下！"
+    exit 1
+  else
+    ss_port=$(grep '"server_port"' /etc/ss-rust/config.json | sed 's/[^0-9]*\([0-9]*\).*/\1/') # 获取 ss-rust 服务的端口号
+  fi
+
   # 获取用户输入的配置信息
   read -r -p "请输入 Shadow-TLS 监听端口 (留空默认随机端口号): " shadow_tls_port
   shadow_tls_port=${shadow_tls_port:-$(shuf -i 10000-30000 -n 1)}
@@ -214,7 +228,7 @@ Restart=on-failure
 RestartSec=5s
 Environment=MONOIO_FORCE_LEGACY_DRIVER=1
 ExecStartPre=/bin/sh -c ulimit -n 51200
-ExecStart=/usr/local/bin/shadow-tls --v3 --strict server --listen 0.0.0.0:${shadow_tls_port} --password ${shadow_tls_password}
+ExecStart=/usr/local/bin/shadow-tls --v3 --strict server --listen 0.0.0.0:${shadow_tls_port} --server 127.0.0.1:${ss_port} --tls gateway.icloud.com --password ${shadow_tls_password}
 StandardOutput=syslog
 StandardError=syslog
 SyslogIdentifier=shadow-tls
